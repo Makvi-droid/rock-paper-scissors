0ebccdaa3a1ef74ee5fb48e9becea7947f8562cc
Makvi-droid <pasbenauro05@gmail.com>
2024-02-17 14:06:14 +0800

    done for now

--- script.js
+++ script.js
@@ -4,46 +4,54 @@ function computerChoice() {
     return choices[randomChoices].toUpperCase();
 }
 
-function round(playerSelection, computerSelection){
-   switch(playerSelection){
 
-    case "rock".toUpperCase():
-        switch(computerSelection){
-            case "rock".toUpperCase():
-                return "It\'s a draw!"
-            case "paper".toUpperCase():
-                return "You lose"
-            case "scissors".toUpperCase():
-                return "You win"
+
+
+    function round(playerSelection, computerSelection){
+        switch(playerSelection){
+     
+         case "rock".toUpperCase():
+             switch(computerSelection){
+                 case "rock".toUpperCase():
+                     return "It\'s a draw!";
+                 case "paper".toUpperCase():
+                     return "You lose";
+                 case "scissors".toUpperCase():
+                     return "You win";
+             }
+             break;
+     
+         case "paper".toUpperCase():
+             switch(computerSelection){
+                 case "rock".toUpperCase():
+                     return "You win";
+                 case "paper".toUpperCase():
+                     return "It\s a draw!";
+                 case "scissors".toUpperCase():
+                     return "You lose";
+             }
+             break;
+     
+         case "scissors".toUpperCase():
+             switch(computerSelection){
+                 case "rock".toUpperCase():
+                     return "You lose";
+                 case "paper".toUpperCase():
+                     return "You win";
+                 case "scissors".toUpperCase():
+                     return "It\s a draw!";
+             }
+             break;
+             default:
+                 return "Invalid, Pls only pick rock paper or scissors";
+     
         }
-        break;
+     }
 
-    case "paper".toUpperCase():
-        switch(computerSelection){
-            case "rock".toUpperCase():
-                return "You win"
-            case "paper".toUpperCase():
-                return "It\s a draw!"
-            case "scissors".toUpperCase():
-                return "You lose"
-        }
-        break;
 
-    case "scissors".toUpperCase():
-        switch(computerSelection){
-            case "rock".toUpperCase():
-                return "You lose"
-            case "paper".toUpperCase():
-                return "You win"
-            case "scissors".toUpperCase():
-                return "It\s a draw!"
-        }
-        break;
-        default:
-            return "Invalid, Pls only pick rock paper or scissors";
 
-   }
-}
+
+
 
 const computerSelection = computerChoice();
 const playerSelection = window.prompt().toUpperCase();

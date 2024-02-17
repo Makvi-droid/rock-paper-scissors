d12671d80e42b6b7349015775b39b2fbe08753a1
Makvi-droid <pasbenauro05@gmail.com>
2024-02-17 13:41:28 +0800

    done making the function to identify who won or lost

--- script.js
+++ script.js
@@ -5,15 +5,49 @@ function computerChoice() {
 }
 
 function round(playerSelection, computerSelection){
-   if(playerSelection != computerSelection){
-        return "you lose";
-    }
-    else{
-        return "you win";
-    }
+   switch(playerSelection){
+
+    case "rock".toUpperCase():
+        switch(computerSelection){
+            case "rock".toUpperCase():
+                return "It\'s a draw!"
+            case "paper".toUpperCase():
+                return "You lose"
+            case "scissors".toUpperCase():
+                return "You win"
+        }
+        break;
+
+    case "paper".toUpperCase():
+        switch(computerSelection){
+            case "rock".toUpperCase():
+                return "You win"
+            case "paper".toUpperCase():
+                return "It\s a draw!"
+            case "scissors".toUpperCase():
+                return "You lose"
+        }
+        break;
+
+    case "scissors".toUpperCase():
+        switch(computerSelection){
+            case "rock".toUpperCase():
+                return "You lose"
+            case "paper".toUpperCase():
+                return "You win"
+            case "scissors".toUpperCase():
+                return "It\s a draw!"
+        }
+        break;
+        default:
+            return "Invalid, Pls only pick rock paper or scissors";
+
+   }
 }
 
-const computerSelection = (computerChoice());
-const playerSelection = prompt.toUpperCase();
+const computerSelection = computerChoice();
+const playerSelection = window.prompt().toUpperCase();
+console.log("Player picked: " + playerSelection)
+console.log("Computer picked: " + computerSelection);
 
 console.log(round(playerSelection, computerSelection));

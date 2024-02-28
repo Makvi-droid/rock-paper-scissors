a7ed5e2f0a8bbf2266eb9fc9069de51b2aa48fb0
Makvi-droid <pasbenauro05@gmail.com>
2024-02-28 12:07:21 +0800

    2nd trial failed

--- script.js
+++ script.js
@@ -5,63 +5,10 @@ const playerPick = document.getElementById("playerPick");
 const computerPick = document.getElementById("computerChoice");
 const displayWinner = document.getElementById("displayWinner");
 
-function computerChoice() {
-    let choices = [btnRock, btnPaper, btnScissors];
-    let randomChoices = Math.floor(Math.random() * choices.length);
-    return choices[randomChoices];
-}
 
 
-function round(playerSelection, computerSelection){
-    switch(playerSelection){
- 
-     case btnRock.addEventListener('click', function(){
-        playerPick.textContent = 'Player picked rock';
-    }):
-         switch(computerSelection){
-             case btnRock:
-                 return displayWinner.textContent = "It/'s a draw";
-             case btnPaper:
-                 return displayWinner.textContent = "You lose";
-             case btnScissors:
-                 return displayWinner.textContent = "You win";
-         }
-         break;
- 
-     case btnPaper.addEventListener('click', function(){
-        playerPick.textContent = 'Player picked paper';
-    }):
-         switch(computerSelection){
-             case btnRock:
-                 return displayWinner.textContent = "You win";
-             case btnPaper:
-                 return displayWinner.textContent = "It/'s a draw";
-             case btnScissors:
-                 return displayWinner.textContent = "You lose";
-         }
-         break;
- 
-     case btnScissors.addEventListener('click', function(){
-        playerPick.textContent = 'Player picked scissors';
-    }):
-         switch(computerSelection){
-             case btnRock:
-                 return displayWinner.textContent = "You lose";
-             case btnPaper:
-                 return displayWinner.textContent = "You win";
-             case btnScissors:
-                 return displayWinner.textContent = "It/'s a draw";
-         }
-         break;
- 
-    }
 
- }
 
- const computerSelection = computerChoice();
- const playerSelection = [btnPaper, btnRock, btnScissors];
-
- round(playerSelection, computerSelection)
 
 
 

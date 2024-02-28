b1545861145d867f246d8dd930019ba80052a8a1
Makvi-droid <pasbenauro05@gmail.com>
2024-02-28 10:49:04 +0800

    added button functionality for div display

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const btnRock = document.querySelector("#rock");
-const btnPaper = document.querySelector("paper");
+const btnPaper = document.querySelector("#paper");
 const btnScissors = document.querySelector("#scissors");
+const playerPick = document.getElementById("displayWinner");
 
 btnRock.addEventListener('click', function(){
-    
+    playerPick.textContent = 'Player picked rock';
 });
 
 btnPaper.addEventListener('click', function(){
-    
+    playerPick.textContent = 'Player picked paper';
 });
 
 btnScissors.addEventListener('click', function(){
-    
+    playerPick.textContent = 'Player picked scissors';
 });
+
+
 
 
 

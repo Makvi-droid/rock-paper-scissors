429a26f65a03a99ed6d7e6989e0174471059b3ac
Makvi-droid <pasbenauro05@gmail.com>
2024-02-28 10:38:15 +0800

    added button functionality to player selection

--- script.js
+++ script.js
@@ -1,69 +1,18 @@
-function computerChoice() {
-    let choices = ['rock', 'paper', 'scissors'];
-    let randomChoices = Math.floor(Math.random() * choices.length);
-    return choices[randomChoices].toUpperCase();
-}
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("paper");
+const btnScissors = document.querySelector("#scissors");
 
-function playGame(){
-
-    for(let i = 1; i <= 5; i++){
-
-        function round(playerSelection, computerSelection){
-            switch(playerSelection){
-         
-             case "rock".toUpperCase():
-                 switch(computerSelection){
-                     case "rock".toUpperCase():
-                         return "It\'s a draw!";
-                     case "paper".toUpperCase():
-                         return "You lose";
-                     case "scissors".toUpperCase():
-                         return "You win";
-                 }
-                 break;
-         
-             case "paper".toUpperCase():
-                 switch(computerSelection){
-                     case "rock".toUpperCase():
-                         return "You win";
-                     case "paper".toUpperCase():
-                         return "It\s a draw!";
-                     case "scissors".toUpperCase():
-                         return "You lose";
-                 }
-                 break;
-         
-             case "scissors".toUpperCase():
-                 switch(computerSelection){
-                     case "rock".toUpperCase():
-                         return "You lose";
-                     case "paper".toUpperCase():
-                         return "You win";
-                     case "scissors".toUpperCase():
-                         return "It\s a draw!";
-                 }
-                 break;
-                 default:
-                     return "Invalid, Pls only pick rock paper or scissors";
-         
-            }
-         }
+btnRock.addEventListener('click', function(){
     
-    const computerSelection = computerChoice();
-    const playerSelection = window.prompt().toUpperCase();
-    console.log("Player picked: " + playerSelection)
-    console.log("Computer picked: " + computerSelection);
+});
+
+btnPaper.addEventListener('click', function(){
     
-    console.log(round(playerSelection, computerSelection));
+});
 
-    }
-    round();
-
-}
-
-playGame();
-
-
+btnScissors.addEventListener('click', function(){
+    
+});
 
 
 

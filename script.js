7a0ff96af2f4a75b918e15ef1729c25a713e3e38
Makvi-droid <pasbenauro05@gmail.com>
2024-03-01 11:51:15 +0800

    revising my code again

--- script.js
+++ script.js
@@ -3,24 +3,26 @@ const btnPaper = document.querySelector('#paper');
 const btnScissors = document.querySelector('#scissors');
 const playerPick = document.getElementById('playerPick');
 const computerChoice = document.getElementById('computerChoice');
-const displayWinner = document.getElementById('displaWinner');
+const displayWinner = document.getElementById('displayWinner');
 const roundWinner = document.getElementById('roundWinner');
 
 const choices = ["rock", "paper", "scissors"];
 
-function playRound(){
-
+function computerPick(){
+    return choices[Math.floor(Math.random() * 3)];
 }
 
-
 btnRock.addEventListener('click', function(){
-    
+    playerPick.textContent = "Player picked rock";
+    computerChoice.textContent = "Compuer picked " + computerPick();
 });
 
 btnPaper.addEventListener('click', function(){
-    
+    playerPick.textContent = "Player picked paper";
+    computerChoice.textContent = "Compuer picked " + computerPick();
 });
 
 btnScissors.addEventListener('click', function(){
-    
+    playerPick.textContent = "Player picked scissors";
+    computerChoice.textContent = "Compuer picked " + computerPick();
 });
